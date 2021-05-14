704a369e8b7039b649fd7d6c07a2996ad39690fd
holounix <60943198+holounix@users.noreply.github.com>
2021-05-14 18:36:41 -0500

    update

--- LineSegment/Main.java
+++ LineSegment/Main.java
@@ -2,8 +2,23 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String [] args){
-        Point[] testArray = generate.generateArray(5);
+        Point[] testArray = generate.generateArray(10);
         System.out.println(Arrays.toString(testArray));
+        
+        Point.sortPoint(testArray);
+        System.out.println(Arrays.toString(testArray));
+        
+        double angleOutput = testArray[0].angle();
+        System.out.println(angleOutput);
+        
+        double angleOutput2 = testArray[1].angle();
+        System.out.println(angleOutput2);
+        
+        Point.collinearPoints(testArray);
+        System.out.println(Arrays.toString(testArray));
+
+        Double[] testAngle = Point.anglesArray(testArray);
+        System.out.println(Arrays.toString(testAngle));
         
     }
 }
--- LineSegment/Point.java
+++ LineSegment/Point.java
@@ -12,7 +12,7 @@ public class Point implements Comparable<Point>{
         if (this.x == b.x){
             return 0;
         }
-        return this.x<b.x?-1:1;
+        return this.x>b.x?1:-1;
     }
     public static Point[] sortPoint(Point[] a){
         MergeSort.mergeSort(a);
@@ -23,20 +23,27 @@ public class Point implements Comparable<Point>{
     }
     
     public double angle(){
-        return Math.atan2(this.y, this.x);
+        return Math.atan2(this.x, this.y);
     }
     public static Point [] collinearPoints(Point[] a){
         List<Point> temp = new ArrayList<Point>();
-        Collections.addAll(temp, a);
-        for (int i = 0; i < temp.size();i++){
-            while (temp.get(i).angle() == temp.get(i+1).angle()){
-                temp.remove(i+1);
+        for (int i = 0; i < a.length-1;i++){
+            if (a[i].angle() == a[i+1].angle()){
+                temp.add(a[i]);
+                temp.add(a[i+1]);
             }
+            break;
         }
         Point[] lineSegment = new Point[temp.size()];
         lineSegment = temp.toArray(lineSegment);
         return lineSegment;
-     
+    }
+    public static Double[] anglesArray(Point[] a){
+        Double[] angles = new Double[a.length];
+        for (int i = 0; i < a.length;i++){
+            angles[i] = a[i].angle(); 
+        }
+        return angles;
     }
 
 
